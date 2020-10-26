71e4bc7c4ac30f30fda8e034633a0d6e4857c608
Mnalbete <mnalbete@uw.edu>
2020-10-26 13:57:04 -0700

    stuff

--- homework 3/Develop/script.js
+++ homework 3/Develop/script.js
@@ -4,8 +4,19 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
+  
+  // possible password values
 
+  let value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890";
+  
+  let password = "";
+
+  // create for loop to choose password characters 
+  for (var i = 0; i <= complexity i++){
+    password = password = value.charAt(Math.floor(Math.random))
+  }
   passwordText.value = password;
 
 }
