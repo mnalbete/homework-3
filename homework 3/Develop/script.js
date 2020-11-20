bb51c83517afe8e58b1d71d36b8f084b8ec835fe
mathew <mnalbete@uw.edu>
2020-11-20 00:59:00 -0800

    1258 11/20/2020

--- homework 3/Develop/script.js
+++ homework 3/Develop/script.js
@@ -1,25 +1,52 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  
-  var passwordText = document.querySelector("#password");
-  
-  // possible password values
+var caseArray = [];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
 
-  let value = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890";
-  
-  let password = "";
 
-  // create for loop to choose password characters 
-  for (var i = 0; i <= complexity i++){
-    password = password = value.charAt(Math.floor(Math.random))
-  }
-  passwordText.value = password;
+generateBtn.addEventListener("click", function (event) {
+  var generateButton = document.getElementById("generate");
+  generatePassword();
+});
 
-}
+function generatePassword() {
+  var passLength = prompt("How long should your password be?");
+  var passUpper = confirm("Should your password include upper case letters?");
+  var passLower = confirm("Should your password include lower case letters?");
+  var passNum = confirm("Should your password include numbers?");
+  var passSpecial = confirm("Should your password include special characters?");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  if (passUpper === true) {
+    caseArray.push(upperCase);
+  };
+
+  if (passLower === true) {
+    caseArray.push(lowerCase);
+  };
+
+  if (passNum === true) {
+    caseArray.push(numbers)
+  };
+
+  if (passSpecial === true) {
+    caseArray.push(specialChar)
+  };
+
+  for (var i = 0; i < passLength; i++) {
+    var randomCharArrayNum;
+    var selectedCharArray;
+    var randomCharNum;
+    var randomChar;
+
+    randomCharArrayNum = parseInt(Math.floor(Math.random() * caseArray.length));
+    selectedCharArray = caseArray[randomCharArrayNum];
+    randomCharNum = Math.floor(Math.random() * selectedCharArray.length);
+    randomChar = selectedCharArray[randomCharNum];
+    passwordString = randomChar;
+  };
+  document.getElementById("password").val= generatePassword;
+};
+
